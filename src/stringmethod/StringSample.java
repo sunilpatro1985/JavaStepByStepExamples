fde5bf34b7ad4f3fe6777f4ba15a6d223ffd8da8
Sunil Kumar Patro <sisintisunil.patro@thomsonreuters.com>
2018-10-18 21:19:20 +0530

    String ex

---
+++ src/stringmethod/StringSample.java
@@ -0,0 +1,11 @@
+package stringmethod;
+
+public class StringSample {
+
+	public static void main(String[] args) {
+		String s1 = "qav";
+		String s2 = "qav";
+		System.out.println(s1 == s2);
+	}
+
+}
